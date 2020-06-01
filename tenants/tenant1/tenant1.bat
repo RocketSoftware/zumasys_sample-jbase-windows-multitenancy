105cf76b3697f87e37dbb8d7047c79cb1b63a4b0
Patrick Payne <patrickp@zumasys.com>
2020-06-01 03:36:45 -0700

    Added files

---
+++ tenants/tenant1/tenant1.bat
@@ -0,0 +1,6 @@
+SET JEDIFILENAME_SYSTEM=c:\dbms\tenants\tenant1\tenant1.system]D
+SET JBCSPOOLERDIR=C:\dbms\tenants\tenant1\spooler
+SET JBCPORTNO=100-199
+SET JBCLISTFILE=c:\dbms\tenants\tenant1\lists
+SET JSH_COMMAND_STACK=USERNAME,1000
+
---
+++ tenants/tenant2/tenant2.bat
@@ -0,0 +1,6 @@
+SET JEDIFILENAME_SYSTEM=c:\dbms\tenants\tenant2\tenant2.system]D
+SET JBCSPOOLERDIR=C:\dbms\tenants\tenant2\spooler
+SET JBCPORTNO=200-299
+SET JBCLISTFILE=c:\dbms\tenants\tenant2\lists
+SET JSH_COMMAND_STACK=USERNAME,1000
+
